d5da20cf36fd4cd4323a0c74fff48a9ec8cbfa11
Bryan Frazar <bryan.frazar@louisvilleky.gov>
2017-04-21 16:26:32 -0400

    Revised publishing details

--- GetSouthernIndianaData/Properties/AssemblyInfo.cs
+++ GetSouthernIndianaData/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GetIDEMData")]
+[assembly: AssemblyTitle("APCD Get IDEM Data")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Louisville Metro Government")]
-[assembly: AssemblyProduct("APCD.AirNow")]
-[assembly: AssemblyCopyright("Copyright © Louisville Metro Government 2016-2017")]
+[assembly: AssemblyCompany("Louisville Metro Air Pollution Control District")]
+[assembly: AssemblyProduct("APCD AirNow Utilities")]
+[assembly: AssemblyCopyright("Copyright © Louisville Metro Air Pollution Control District 2016-2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
